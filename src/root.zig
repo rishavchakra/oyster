c313d50da7ca536c67058f34f819d25a793dc1c6
Rishav Chakravarty <rishavchakra@gmail.com>
2026-03-16 20:13:47 -0400

    Zig cleanup

--- src/root.zig
+++
@@ -1,9 +0,0 @@
-pub const parser = @import("parser.zig");
-pub const compiler = @import("compiler.zig");
-pub const runtime = @import("runtime.zig");
-
-test {
-    _ = @import("parser.zig");
-    _ = @import("compiler.zig");
-    _ = @import("runtime.zig");
-}
